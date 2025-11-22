5bfc5380126c75dd0bfca712a2631c91fa9a3fa2
axolotl-go <kalelcg.eg@gmail.com>
2025-11-22 11:22:35 -0600

    feat: Enhance userController to return user details in index method and hash passwords in store method

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,6 @@ use App\Http\Controllers\userController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get("/GASW", [userController::class, 'index']);
+Route::get("/users", [userController::class, 'index']);
 
 Route::post("/user", [userController::class, 'store']);
